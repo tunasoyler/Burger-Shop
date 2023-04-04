9f99d3d3402fa40a74018c4ac90e0b3d15a9d54a
Tuna Soyler <tunasoyler@gmail.com>
2023-04-04 16:54:41 +0300

    .

--- Entity/Concrete/Menu.cs
+++ Entity/Concrete/Menu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entity.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,10 +8,10 @@ using System.Threading.Tasks;
 namespace Entity.Concrete
 {
     public class Menu : BaseEntity
-    {
-        public string Burger { get; set; }        
+    {       
         public byte[] Image { get; set; }
         public string Description { get; set; }
+        public Size Size { get; set; }
         public decimal Price { get; set; }
         public ICollection<OrderDetails> OrderDetails { get; set; }
         public Menu()
