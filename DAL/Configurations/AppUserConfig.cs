1480823c8e625732bd67bf58d876372e42e9d174
Tuna Soyler <tunasoyler@gmail.com>
2023-04-04 22:38:25 +0300

    .

--- DAL/Configurations/AppUserConfig.cs
+++ DAL/Configurations/AppUserConfig.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MVC.Models.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,23 @@ using System.Threading.Tasks;
 
 namespace DAL.Configurations
 {
-    internal class AppUserConfig
+    public class AppUserConfig : IEntityTypeConfiguration<AppUser>
     {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.ToTable("Users");
+
+            builder.Property(u => u.FirstName).IsRequired().HasMaxLength(50);
+            builder.Property(u => u.LastName).IsRequired().HasMaxLength(50);
+            builder.Property(u => u.AddressLine).IsRequired().HasMaxLength(100);
+            builder.Property(u => u.City).IsRequired().HasMaxLength(50);
+            builder.Property(u => u.Country).IsRequired().HasMaxLength(50);
+            builder.Property(u => u.MemberSince).HasConversion(typeof(DateTime)).HasDefaultValue(DateTime.Now);
+
+            builder.HasMany(u => u.Orders)
+               .WithOne(o => o.User)
+               .HasForeignKey(o => o.UserId)
+               .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
--- DAL/Configurations/ExtraConfig.cs
+++ DAL/Configurations/ExtraConfig.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,25 @@ using System.Threading.Tasks;
 
 namespace DAL.Configurations
 {
-    internal class ExtraConfig
+    public class ExtraConfig : IEntityTypeConfiguration<Extra>
     {
+        public void Configure(EntityTypeBuilder<Extra> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(30);
+            builder.Property(x => x.Price).IsRequired();
+            builder.Property(x => x.CreatedTime).HasConversion(typeof(DateTime)).HasDefaultValue(DateTime.Now);
+            builder.Property(x => x.ModifiedTime).HasConversion(typeof(DateTime)).HasDefaultValue(DateTime.Now);
+
+            builder.HasOne(x => x.ExtraCategory)
+                .WithMany(c => c.Extras)
+                .HasForeignKey(x => x.ExtraCategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(x => x.OrderDetails)
+                .WithOne(od => od.Extra)
+                .HasForeignKey(od => od.ExtraId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
