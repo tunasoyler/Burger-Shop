ab8a43ff6258fc7cc5cfa7d2a96d03a1bb97d43f
Beste Yasak <beyasak@hotmail.com>
2023-04-05 10:46:53 +0300

    //contactLayout2

---
+++ MVC/Controllers/PaymentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetPayment()
+        { 
+            return View(); 
+        }
+
+    }
+}
---
+++ MVC/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+	public class UserController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult GetOrders()
+		{ 
+			return View(); 
+		}
+		public IActionResult Login() 
+		{ 
+
+		}
+		
+	}
+}
