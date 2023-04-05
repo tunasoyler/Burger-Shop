bcf9d8967da7c37940f3f37e8c265cadcee20c8f
glrmnhuseyin <125462131+glrmnhuseyin@users.noreply.github.com>
2023-04-05 10:28:56 +0300

    .

---
+++ DAL/EntityFramework/EfExtraCategoryDal.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Abstract;
+using DAL.Concrete.Repositories;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EntityFramework
+{
+    public class EfExtraCategoryDal:GenericRepository<ExtraCategory>,IExtraCategory
+    {
+    }
+}
