2d174b41b1379165f2f77861b3c144a31692123d
glrmnhuseyin <125462131+glrmnhuseyin@users.noreply.github.com>
2023-04-05 22:45:26 +0300

    .

--- MVC/Controllers/AdminController.cs
+++ MVC/Controllers/AdminController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models.Context;
 
 namespace MVC.Controllers
 {
 	public class AdminController : Controller
 	{
+		private UserManager<AppUser> userManager;
+		public AdminController(UserManager<AppUser> _userManager)
+		{
+			userManager=_userManager;
+
+		}
 		public IActionResult Index()
 		{
 			return View();
+		}
+		public IActionResult GetUser()
+		{
+			return View(userManager.Users);
 		}
 	}
 }
