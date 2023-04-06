87b431d69d30595dd389cbf679e7544be66a26de
Tuna Soyler <tunasoyler@gmail.com>
2023-04-06 12:15:16 +0300

    .

--- SeedData/Program.cs
+++ SeedData/Program.cs
@@ -47,23 +47,23 @@ namespace SeedData
 
 
             List<Menu> menus = new List<Menu>() {
-                new Menu {  Name = "BBQ Burger", Price = 80, Image = ReadFile("Resources/bbq.png"), Description="", MenuCategory="etmenu" },
-                new Menu {  Name = "Big Mac", Price = 90, Image = ReadFile("Resources/bigmac.png"), Description="", MenuCategory="etmenu" },
-                new Menu {  Name = "Sakura 2'li Burger", Price = 100, Image = ReadFile("Resources/ikiliSakura.png"), Description="" , MenuCategory="etmenu"},
-                new Menu {  Name = "Sakura 3'lü Burger", Price = 120, Image = ReadFile("Resources/ucluSakura.png"), Description="" , MenuCategory="etmenu"},
-                new Menu {  Name = "Doktor Burger", Price = 100, Image = ReadFile("Resources/doktor.png"), Description="", MenuCategory="etmenu" },
-                new Menu {  Name = "Cheese Burger", Price = 70, Image = ReadFile("Resources/cheese.png"), Description="", MenuCategory="etmenu" },                
-                new Menu {  Name = "Doyamıyorum Menüsü", Price = 150, Image = ReadFile("Resources/doyamiyorum.png"), Description="", MenuCategory="etmenu" },                
-                new Menu {  Name = "Toplu Menü", Price = 140, Image = ReadFile("Resources/toplu.png"), Description="", MenuCategory="etmenu" },
-                new Menu {  Name = "Klasik Menü", Price = 100, Image = ReadFile("Resources/klasik.png"), Description="", MenuCategory="etmenu" },
+                new Menu {  Name = "BBQ Burger", Price = 80, Image = ReadFile("Resources/bbq.png"), Description="180 gr. dana burger köftesi, kıtır soğan, sotelenmiş mantar, dana bacon, cheddar peyniri, BBQ sos, patates kızartması.", MenuCategory="etmenu" },
+                new Menu {  Name = "Big Mac", Price = 90, Image = ReadFile("Resources/bigmac.png"), Description="İki katmanlı özel hamburger ekmeği arasında közde marine edilmiş sığır eti köftesi, çedar peyniri, turşu, soğan, turşulu mayonez ve marul ile servis edilir.", MenuCategory="etmenu" },
+                new Menu {  Name = "Sakura 2'li Burger", Price = 100, Image = ReadFile("Resources/ikiliSakura.png"), Description="İki adet köftesiyle özel bir lezzet sunan burger. Dana köftesi, hardal, soya sosu, teriyaki sos, cheddar peyniri, marul, domates ve turşu eşliğinde servis edilir." , MenuCategory="etmenu"},
+                new Menu {  Name = "Sakura 3'lü Burger", Price = 120, Image = ReadFile("Resources/ucluSakura.png"), Description="Üç adet köftesiyle özel bir lezzet sunan burger. Dana köftesi, hardal, soya sosu, teriyaki sos, cheddar peyniri, marul, domates ve turşu eşliğinde servis edilir." , MenuCategory="etmenu"},
+                new Menu {  Name = "Doktor Burger", Price = 100, Image = ReadFile("Resources/doktor.png"), Description="Dana burger köftesi, mantar, hardal, ketchup, cheddar peyniri, marul, domates, turşu, tursu suyu ve ketçaplı özel sos ile servis edilir.", MenuCategory="etmenu" },
+                new Menu {  Name = "Cheese Burger", Price = 70, Image = ReadFile("Resources/cheese.png"), Description="Dana burger köftesi, cheddar peyniri, marul, domates, turşu ve klasik burger sosu ile servis edilir.", MenuCategory="etmenu" },                
+                new Menu {  Name = "Doyamıyorum Menüsü", Price = 150, Image = ReadFile("Resources/doyamiyorum.png"), Description="Açlığınızı sonlandırmak için en ideal seçenek. 2 adet BBQ Burger, 2 adet tavuk kanadı, patates kızartması ve 2 adet içecek ile servis edilir.", MenuCategory="etmenu" },                
+                new Menu {  Name = "Toplu Menü", Price = 140, Image = ReadFile("Resources/toplu.png"), Description="3 adet Cheese Burger, 3 adet tavuk kanadı, büyük boy patates kızartması ve 1 litrelik içecek ile servis edilir.", MenuCategory="etmenu" },
+                new Menu {  Name = "Klasik Menü", Price = 100, Image = ReadFile("Resources/klasik.png"), Description="Dana burger köftesi, marul, domates, turşu, klasik burger sosu ve patates kızartması ile servis edilir.", MenuCategory="etmenu" },
 
-                new Menu {  Name = "Fish Burger", Price = 80, Image = ReadFile("Resources/fishMenu.png"), Description="", MenuCategory="balikmenu" },
-                new Menu {  Name = "Double Fish Burger", Price = 100, Image = ReadFile("Resources/doubleFish.png"), Description="", MenuCategory="balikmenu" },
+                new Menu {  Name = "Fish Burger", Price = 80, Image = ReadFile("Resources/fishMenu.png"), Description="Lezzetli balık filetosu, marul, domates, turşu ve tartar sos ile servis edilir.", MenuCategory="balikmenu" },
+                new Menu {  Name = "Double Fish Burger", Price = 100, Image = ReadFile("Resources/doubleFish.png"), Description="İki katmanlı lezzetli balık filetosu, marul, domates, turşu ve tartar sos ile servis edilir", MenuCategory="balikmenu" },
 
-                new Menu {  Name = "Tavuk Burger", Price = 70, Image = ReadFile("Resources/tavuk.png"), Description="", MenuCategory="tavukmenu" },
-                new Menu {  Name = "Izgara Tavuk Burger", Price = 100, Image = ReadFile("Resources/izgaraTavuk.png"), Description="", MenuCategory="tavukmenu" },
+                new Menu {  Name = "Tavuk Burger", Price = 70, Image = ReadFile("Resources/tavuk.png"), Description="Tavuk göğsü, cheddar peyniri, marul, domates, turşu ve mayonez ile servis edilir.", MenuCategory="tavukmenu" },
+                new Menu {  Name = "Izgara Tavuk Burger", Price = 100, Image = ReadFile("Resources/izgaraTavuk.png"), Description="Izgara tavuk göğsü, cheddar peyniri, marul, domates, turşu ve baharatlı sos ile servis edilir.", MenuCategory="tavukmenu" },
 
-                new Menu {  Name = "Vegan Burger", Price = 70, Image = ReadFile("Resources/vegan.png"), Description="", MenuCategory="veganmenu" }};
+                new Menu {  Name = "Vegan Burger", Price = 70, Image = ReadFile("Resources/vegan.png"), Description="Vegan burger köftesi, vegan cheddar peyniri, marul, domates, turşu ve vegan mayonez ile servis edilir.", MenuCategory="veganmenu" }};
 
 
 
